5259d72447388cb752577214e0720359602e0d92
KevinBaudson <kevinbaudson@hotmail.com>
2024-04-26 11:15:26 -0300

    integrando api google books

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
+
+async function getBooks(){
+  const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=search+terms&AIzaSyAnZRFEoXZmsm8U_zzUkLoBB3ImF0agCss")
+  const data = await response.json()
+  const items = data.items
+
+  console.log(items)
+    
+  items.map((item)=>{
+    let bookTitle = item['volumeInfo']['title']
+    let bookImage = item['volumeInfo']['imageLinks']
+    let image =  bookImage.thumbnail
+    let livrosDestaque = document.querySelector('.destaque')
+    let imgDestaque = document.createElement('img')
+    imgDestaque.src = `${image}`
+    livrosDestaque.appendChild(imgDestaque)
+  })
+ }
   
+
+
+getBooks()
+
+
+
   function showMenu(){
     const body = document.querySelector('body')
     const navMobile = document.querySelector('#nav-mobile');
@@ -13,3 +37,10 @@
       navMobile.style.display = 'none'    
     }
   }
+
+
+  const btnElement = document.querySelector('.btn-search');
+  btnElement.addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log('clicou')
+  })
